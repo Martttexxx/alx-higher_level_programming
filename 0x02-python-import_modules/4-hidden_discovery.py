0e723231c03c08f58334446bfb3e3c98a5e46061
Martttexxx <martinnwosu601@gmail.com>
2024-01-26 06:26:04 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from hidden_4 import *
-   name_f = dir()
-   for in range(0, len(all_f)):
-       if all_f[i][:2] != "__":
-           print("{:s}".format(all_f[i]))
+    name_f = dir()
+    for i in range(0, len(all_f)):
+        if all_f[i][:2] != "__":
+            print("{:s}".format(all_f[i]))
